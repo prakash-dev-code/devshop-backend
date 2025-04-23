3f97fa10c2c4f19714b90b28610f25f5b0ea096b
prakash-ca <prakash.sahu@cognyx.ai>
2025-04-23 14:47:38 +0530

    OTP signup template added

--- controllers/authController.js
+++ controllers/authController.js
@@ -86,7 +86,8 @@ exports.googleLogin = catchAsync(async (req, res, next) => {
   const payload = ticket.getPayload();
   const { email, name, picture, sub: googleId } = payload;
 
-  if (!email) return next(new appError("Email not found in Google account", 400));
+  if (!email)
+    return next(new appError("Email not found in Google account", 400));
 
   let user = await User.findOne({ email });
 
@@ -121,7 +122,151 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   const otp = generateOTP(); // e.g., 6-digit code
   const otpExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
-  const message = `Your OTP for email verification is: ${otp}`;
+  // const message = `Your OTP for email verification is: ${otp}`;
+  const message = `<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+  xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="x-apple-disable-message-reformatting">
+  <title>Reset Password</title>
+
+  <!-- Web Font / @font-face : BEGIN -->
+  <!--[if mso]>
+    <style>
+      * {
+        font-family: 'Roboto', sans-serif !important;
+      }
+    </style>
+  <![endif]-->
+
+  <!--[if !mso]><!-->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
+  <!--<![endif]-->
+
+  <style>
+    html,
+    body {
+      margin: 0 auto !important;
+      padding: 0 !important;
+      height: 100% !important;
+      width: 100% !important;
+      font-family: 'Roboto', sans-serif !important;
+      font-size: 14px;
+      margin-bottom: 10px;
+      line-height: 24px;
+      color: #8094ae;
+      font-weight: 400;
+    }
+
+    * {
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+      margin: 0;
+      padding: 0;
+    }
+
+    table,
+    td {
+      mso-table-lspace: 0pt !important;
+      mso-table-rspace: 0pt !important;
+    }
+
+    table {
+      border-spacing: 0 !important;
+      border-collapse: collapse !important;
+      table-layout: fixed !important;
+      margin: 0 auto !important;
+    }
+
+    table table table {
+      table-layout: auto;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    img {
+      -ms-interpolation-mode: bicubic;
+    }
+  </style>
+</head>
+
+<body width="100%" style="margin: 0; padding: 0 !important; background-color: #f5f6fa; mso-line-height-rule: exactly;">
+
+  <!-- Start Preheader -->
+  <div class="preheader"
+    style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+    Forgot your password? No worries — click the button below to reset it securely.
+  </div>
+  <!-- End Preheader -->
+
+  <center style="width: 100%; background-color: #f5f6fa;">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#f5f6fa">
+      <tr>
+        <td style="padding: 40px 0;">
+
+          <!-- Logo Section -->
+          <table style="width:100%;max-width:620px;margin:0 auto;">
+            <tr>
+              <td style="text-align: center; padding-bottom:25px">
+                <a href="#">
+                  <h2>Dev Shop</h3>
+                </a>
+              </td>
+            </tr>
+          </table>
+
+          <!-- Main Content Section -->
+          <table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;">
+            <tr>
+              <td style="text-align:center;padding: 30px 30px 15px 30px;">
+                <h2 style="font-size: 18px; color: #6576ff; font-weight: 600; margin: 0;">Verify your email</h2>
+              </td>
+            </tr>
+            <tr>
+              <td style="text-align:center;padding: 0 30px 20px">
+
+                <p style="margin-bottom: 25px;">Copy the OTP below to complete your verification.</p>
+               <pstyle="font-size: 24px; font-weight: 600; color: #6576ff;">${otp}</p>
+              </td>
+            </tr>
+            <tr>
+              <td style="text-align:center;padding: 20px 30px 40px">
+                <p>If you did not request this, please contact us or ignore this message.</p>
+                <p style="margin: 0; font-size: 13px; line-height: 22px; color:#9ea8bb;">
+                  This is an automatically generated email. Please do not reply to this email. If you face any issues,
+                  please contact us at
+                  <a href="#" style="color: #6576ff;">devshop@gmail.com</a>
+                </p>
+              </td>
+            </tr>
+          </table>
+
+          <!-- Footer -->
+          <table style="width:100%;max-width:620px;margin:0 auto;">
+            <tr>
+              <td style="text-align: center; padding:25px 20px 0;">
+                <p style="font-size: 13px;">
+                  Copyright © 2025 Dev Shop. All rights reserved.
+
+                </p>
+              </td>
+            </tr>
+          </table>
+
+        </td>
+      </tr>
+    </table>
+  </center>
+</body>
+
+</html>
+`;
 
   const existing = await User.findOne({ email });
 
@@ -141,7 +286,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   await sendEmail({
     email: newUser.email,
     subject: "Email Verification OTP - valid for 10 minutes",
-    text: message,
+    html: message,
   });
 
   res.status(201).json({
@@ -412,7 +557,6 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   const userDoc = { id: _id, ...rest };
 
   //4.  Log the user in ,send the JWT token
-
 
   res.status(201).json({
     status: "success",
